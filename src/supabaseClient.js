d71368da231007666f5b2783ba3c962145a47d7b
mmarronetravels-sudo <mmarronetravels@gmail.com>
2026-03-04 07:51:30 -0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://fgbigyffgzqzvksrkqxv.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnYmlneWZmZ3pxenZrc3JrcXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NTY5OTYsImV4cCI6MjA4NTUzMjk5Nn0.GCdHMlWABSNtHdTHMicIXzD3AqPMiBMlQN57GRDMqEc'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)  
